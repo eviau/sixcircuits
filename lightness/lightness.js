1a4c1122f065f7fd71c1cea25faf896c3daad410
eviau <37245459+eviau@users.noreply.github.com>
2020-08-23 11:44:00 -0400

    lightness step 2: adding suggestions

--- lightness/lightness.js
+++ lightness/lightness.js
@@ -1,8 +1,14 @@
 var objects = {}
 
+var suggestions_obj = ["an apple seed","the tear of joy of a fairy","the space between thoughts"];
+var suggestions_weight = ["a pencil, at most","2 or 3 grain of salt","my two cents"];
+
 var current_obj = ''
 
 var ieme = 1;
+
+var ieme_obj = 0;
+var ieme_w = 0;
 
 document.getElementById('nameobj').value = '';
 document.getElementById('wobj').value = '';
@@ -37,4 +43,24 @@ function displayobjects() {
     console.log(objects);
     divi.innerHTML = ieme + ": " + objects[ieme].name + ", " + objects[ieme].weight;
     objectstable.appendChild(divi);
+}
+
+dragElement(document.getElementById("mydivheader"))
+
+function dragElement(elem){
+
+}
+
+function suggestobj(){
+    document.getElementById('nameobj').value = suggestions_obj[ieme_obj];
+    ieme_obj += 1;
+    ieme_obj = ieme_obj % suggestions_obj.length;
+
+}
+
+function suggestw(){
+    document.getElementById('wobj').value = suggestions_weight[ieme_w];
+    ieme_w += 1;
+    ieme_w = ieme_w % suggestions_weight.length;
+
 }
